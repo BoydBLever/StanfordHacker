4c26c411f23292149d902af0cbc07f44effabda1
Boyd Lever <boydlever@stanfordhacker.com>
2022-12-18 16:29:26 -0800

    Initial commit

---
+++ src/main/java/com/boydlever/stanfordhacker/StanfordhackerApplication.java
@@ -0,0 +1,13 @@
+package com.boydlever.stanfordhacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StanfordhackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StanfordhackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boydlever/beltexam/StanfordhackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boydlever.beltexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanfordhackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
